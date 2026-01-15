d1e67fec02678e65e291b4c4e6910cea129ddacc
Hamza-Nawabi <hamza.nawabi119@gmail.com>
2026-01-15 08:44:12 +0430

    update footer of authentication pages

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -15,7 +15,7 @@ const AuthLayout = () => {
         <Outlet />
       </div>
       <div className='bg-white text-slate-400 p-4 mt-4 rounded-md flex flex-col w-full shadow max-w-md justify-center items-center'>
-          <p>Developed By: <a className='text-sky-300' href="https://paikarsoft.netlify.app/">PaikarSoft</a></p>
+          <p>Developed By: <a className='text-sky-300' href="https://paikarsoft.netlify.app/">Paikar-ICT</a></p>
           <p>&copy; 2024 - {new Date().getFullYear()} | All rights reserved by</p>
       </div>
       {/* <p className='mt-2'>Developed By <a className='text-sky-400' href="https://paikarsoft.netlify.app/">PaikarSoft</a></p> */}
